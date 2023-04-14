0c353229eda3fd02e2fea3266a0332a36dd2d715
jinki <ekdrnzld@gmail.com>
2023-04-14 19:01:57 +0900

    4/14

--- src/main/java/com/example/webpos/CheckController.java
+++ src/main/java/com/example/webpos/CheckController.java
@@ -4,6 +4,7 @@ package com.example.webpos;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -14,5 +15,12 @@ public class CheckController {
     @GetMapping("/check")
     public String check(String word) {
         return "OK";
+    }
+
+    @GetMapping("/test")
+    public String test(@RequestBody TestRequest request) {
+        request.validationTagNickname();
+
+        return "tagNickName = " + request.getTagNickname() + " " + "content : " + request.getContent();
     }
 }
